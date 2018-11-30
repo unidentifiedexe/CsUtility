e5aa6892831fb2fcba0fd526f9122a0e1b69910e
Orita Yuma <u462231h@osaka-u.ecs.ac.jp>
2018-11-30 15:29:22 +0900

    Rename namespace

--- CsUtility/Enumerable/EnumerableUttilityAsAuto.cs
+++ CsUtility/Enumerable/EnumerableUttilityAsAuto.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Linq;
 
-namespace CsUtility.EnumerableUtillity
+namespace CsUtility.Enumerable
 {
 
     public static partial class EnumerableUtility
