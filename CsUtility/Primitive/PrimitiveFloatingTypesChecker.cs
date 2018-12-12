2dab2176a5b98b9887fafe4c321254cddf97af03
Andy <u462231h@osaka-u.ecs.ac.jp>
2018-12-12 12:24:34 +0900

    PrimitiveUtilitiesクラスにXmlコメントを追加

--- CsUtility/Primitive/PrimitiveFloatingTypesChecker.cs
+++ CsUtility/Primitive/PrimitiveFloatingTypesChecker.cs
@@ -8,48 +8,104 @@ namespace CsUtility.Primitive
     public static partial class PrimitiveUtilities
     {
 
+        /// <summary>
+        /// 指定した値が非数値 (<see cref="float.NaN"/>) かどうかを示す値を返します。
+        /// </summary>
+        /// <param name="val"> 評価する値。 </param>
+        /// <returns>
+        /// <paramref name="val"/> が非数 (<see cref="float.NaN"/>) と評価される場合は true。それ以外の場合は false。
+        /// </returns>
         public static bool IsNaN(this float val)
         {
             return float.IsNaN(val);
         }
 
 
+        /// <summary>
+        /// 指定した値が負または正の無限大と評価されるかどうかを示す値を返します。
+        /// </summary>
+        /// <param name="val"> 評価する値。 </param>
+        /// <returns>
+        /// <paramref name="val"/> が <see cref="float.PositiveInfinity"/> または <see cref="float.NegativeInfinity"/> と評価される場合は true。それ以外の場合は false。
+        /// </returns>
         public static bool IsInfinity(this float val)
         {
             return float.IsInfinity(val);
         }
 
 
+        /// <summary>
+        /// 指定した値が正の無限大と評価されるかどうかを示す値を返します。
+        /// </summary>
+        /// <param name="val"> 評価する値。 </param>
+        /// <returns>
+        /// <paramref name="val"/> が <see cref="float.PositiveInfinity"/> と評価される場合は true。それ以外の場合は false。
+        /// </returns>
         public static bool IsPositiveInfinity(this float val)
         {
             return float.IsPositiveInfinity(val);
         }
 
 
+        /// <summary>
+        /// 指定した値が負の無限大と評価されるかどうかを示す値を返します。
+        /// </summary>
+        /// <param name="val"> 評価する値。 </param>
+        /// <returns>
+        /// <paramref name="val"/> が <see cref="float.NegativeInfinity"/> と評価される場合は true。それ以外の場合は false。
+        /// </returns>
         public static bool IsNegativeInfinity(this float val)
         {
             return float.IsNegativeInfinity(val);
         }
 
 
+        /// <summary>
+        /// 指定した値が非数値 (<see cref="double.NaN"/>) かどうかを示す値を返します。
+        /// </summary>
+        /// <param name="val"> 評価する値。 </param>
+        /// <returns>
+        /// <paramref name="val"/> が非数 (<see cref="double.NaN"/>) と評価される場合は true。それ以外の場合は false。
+        /// </returns>
         public static bool IsNaN(this double val)
         {
             return double.IsNaN(val);
         }
 
 
+        /// <summary>
+        /// 指定した値が負または正の無限大と評価されるかどうかを示す値を返します。
+        /// </summary>
+        /// <param name="val"> 評価する値。 </param>
+        /// <returns>
+        /// <paramref name="val"/> が <see cref="double.PositiveInfinity"/> または <see cref="double.NegativeInfinity"/> と評価される場合は true。それ以外の場合は false。
+        /// </returns>
         public static bool IsInfinity(this double val)
         {
             return double.IsInfinity(val);
         }
 
 
+        /// <summary>
+        /// 指定した値が正の無限大と評価されるかどうかを示す値を返します。
+        /// </summary>
+        /// <param name="val"> 評価する値。 </param>
+        /// <returns>
+        /// <paramref name="val"/> が <see cref="double.PositiveInfinity"/> と評価される場合は true。それ以外の場合は false。
+        /// </returns>
         public static bool IsPositiveInfinity(this double val)
         {
             return double.IsPositiveInfinity(val);
         }
 
 
+        /// <summary>
+        /// 指定した値が負の無限大と評価されるかどうかを示す値を返します。
+        /// </summary>
+        /// <param name="val"> 評価する値。 </param>
+        /// <returns>
+        /// <paramref name="val"/> が <see cref="double.NegativeInfinity"/> と評価される場合は true。それ以外の場合は false。
+        /// </returns>
         public static bool IsNegativeInfinity(this double val)
         {
             return double.IsNegativeInfinity(val);
