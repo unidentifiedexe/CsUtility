70be27bc668d05a2e434bc1f6de4148a290c3cef
Andy <u462231h@osaka-u.ecs.ac.jp>
2019-05-02 15:31:40 +0900

    Conperter.csをわすれてたので追加

---
+++ CsUtility/Comparer/Comparer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsUtility.Comparer
+{
+    /// <summary> 降順(逆順)に並べ替えるための比較子を提供します。 </summary>
+    /// <typeparam name="T"></typeparam>
+    public class DescendingComparer<T> : IComparer<T>
+    {
+        private static DescendingComparer<T> _default;
+
+        private IComparer<T> _base;
+
+        /// <summary>
+        /// 汎用引数で指定された型の順序比較子の逆順並べ替えの既定値を返します。
+        /// </summary>
+        public static DescendingComparer<T> Default
+        {
+            get
+            {
+                if (_default == null)
+                    _default = new DescendingComparer<T>(Comparer<T>.Default);
+                return _default;
+            }
+        }
+
+        /// <summary> 指定した比較子を使用して、降順の比較子を作成します。 </summary>
+        /// <param name="baseComparer"> 基準となる昇順の比較子 </param>
+        /// <returns> 生成された降順比較子 </returns>
+        public DescendingComparer<T> Create(IComparer<T> baseComparer)
+        {
+            if (baseComparer == null)
+                Error.ArgumentNull(nameof(baseComparer));
+            return new DescendingComparer<T>(baseComparer);
+        }
+
+        private DescendingComparer(IComparer<T> baseComparer)
+        {
+            _base = baseComparer;
+        }
+
+        int IComparer<T>.Compare(T x, T y)
+        {
+            return - _base.Compare(x, y);
+        }
+    }
+}
---
+++ UtilTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsUtility.Enumerable;
+
+namespace UtilTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var arr = new[] { 3, 3453, 12, 534, 2, 432, 6, 234, 231 };
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+            for (int i = arr.Length - 1; i >=0 ; i--)
+            {
+                Console.WriteLine(arr[i]);
+            }
+
+            var newArr = arr.ZipVicinity((p, n) => Math.Abs(p - n)).ToArray();
+
+            Console.WriteLine(String.Join("," ,newArr));
+            Console.ReadLine();
+        }
+    }
+}
